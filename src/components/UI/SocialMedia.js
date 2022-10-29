bc062da72f6040149bbb0d1dd62c7b049c99e03b
Nuno Rifa <nuno180202@gmail.com>
2022-10-29 15:17:57 +0700

    Tidy Up code

--- src/components/UI/SocialMedia.js
+++ src/components/UI/SocialMedia.js
@@ -7,26 +7,29 @@ import classes from "./SocialMedia.module.css";
 const SocialMedia = (props) => {
   let social = [
     {
-      id: "1",
+      id: 1,
       name: "Instagram",
       url: instagram,
+      link: "https://www.instagram.com/nuno.rifa_",
     },
     {
-      id: "2",
+      id: 2,
       name: "Facebook",
       url: facebook,
+      link: "https://www.facebook.com/nuno.fadilah",
     },
     {
-      id: "3",
+      id: 3,
       name: "Github",
       url: github,
+      link: "https://github.com/NunoRifa",
     },
   ];
 
   return (
     <div className={classes["social-media"]}>
       {social.map((list) => (
-        <a href="javascript:void(0)" key={list.id}>
+        <a href={list.link} target="_blank" key={list.id}>
           <img src={list.url} alt={list.name} />
         </a>
       ))}
